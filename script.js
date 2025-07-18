8791e621d36acd1c31bf035a6dbcb3d6ccc89045
Samim Islam <110116844+Its-SK@users.noreply.github.com>
2025-07-18 15:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ startButton.addEventListener("click", () => {
 });
 
 function yesButtonListener(){
-    document.body.innerHTML = "<div class='wrapper'><h2 class='question'>I Love Youuuuuuuuuuuuu ❤️, "+input+ "!</h2>"+
+    document.body.innerHTML = "<div class='wrapper'><h2 class='question'>Meet you soon ❤️, "+input+ "!</h2>"+
     "<img class='gif' alt='gif' src='https://media.giphy.com/media/fHGl1MDMNkO6fOaFDF/giphy.gif'/></div><script src='scriptMain.js'></script>"
     gifMain.src = "https://media.giphy.com/media/fHGl1MDMNkO6fOaFDF/giphy.gif";
 }
